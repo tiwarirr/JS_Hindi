21536470a4507aa778592c9c7e8ac645d079fa1e
RADHA RAMAN TIWARI <70423309+tiwarirr@users.noreply.github.com>
2023-08-27 09:43:29 +0000

    variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,22 @@
+const accountId = 10011971
+let accountEmail="abc@gmail.com"
+var accountPassword="***123"
+accountCity="Lucknow"
+
+
+// accountId=2.  //not allowed
+
+console.log(accountId);
+
+accountEmail="newmail@gmail.com"
+accountPassword="*****sardarji"
+accountCity="Etawah"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/* 
+Never use var scope problem. Use only const which cant change and let for declaring variables.
+Ok 
+Yes sir
+
+*/
